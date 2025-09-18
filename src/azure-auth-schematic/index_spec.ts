d7217344c781e243d80d140ac08896da82c756ad
Daniel Olvera <139163393+danOIntellitect@users.noreply.github.com>
2025-09-18 15:37:20 -0700

    fix: test now is configured to find the expected files

--- src/azure-auth-schematic/index_spec.ts
+++ src/azure-auth-schematic/index_spec.ts
@@ -1,14 +1,27 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
+import { Tree } from "@angular-devkit/schematics";
+import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
+import * as path from "path";
 
-const collectionPath = path.join(__dirname, '../collection.json');
+const collectionPath = path.join(__dirname, "../collection.json");
 
-describe('azure-auth-schematic', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematic('azure-auth-schematic', {}, Tree.empty());
+describe("azure-auth-schematic", () => {
+  it("works", async () => {
+    const runner = new SchematicTestRunner("schematics", collectionPath);
+    const tree = await runner.runSchematic(
+      "azure-auth-schematic",
+      {},
+      Tree.empty()
+    );
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual([
+      "/src/app/app.config.authentication.ts",
+      "/src/app/app.config.ts",
+      "/src/app/app.html",
+      "/src/app/app.routes.ts",
+      "/src/app/app.ts",
+      "/src/app/environments/environment.ts",
+      "/src/app/sample/sample.ts",
+      "/src/app/services/authentication.service.ts",
+    ]);
   });
 });
